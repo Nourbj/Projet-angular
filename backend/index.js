f74208f81902d7ca755d591de417447a98d826b3
hadil <hadilyahiaoui02@gmail.com>
2023-12-28 16:18:12 +0100

    Updated index?js

--- backend/index.js
+++ backend/index.js
@@ -28,66 +28,60 @@ function startServer() {
         email: String,
         password: String
     });
-// Route to handle signup
-app.post('/signup', async (req, res) => {
-    const { username, email, password } = req.body;
+    
+    // Route to handle signup
+    app.post('/signup', async (req, res) => {
+        const { username, email, password } = req.body;
 
-    try {
-        // Hash the password before saving it to the database
-        const hashedPassword = await bcrypt.hash(password, 10);
+        try {
+            // Hash the password before saving it to the database
+            const hashedPassword = await bcrypt.hash(password, 10);
 
-        // Create a new instance of the User model with the hashed password
-        const newUser = new User({ username, email, password: hashedPassword });
+            // Create a new instance of the User model with the hashed password
+            const newUser = new User({ username, email, password: hashedPassword });
 
-        // Save to the database
-        const savedUser = await newUser.save();
+            // Save to the database
+            const savedUser = await newUser.save();
 
-        console.log('User signed up successfully:', savedUser);
-        res.status(201).json({ message: 'Signup successful' });
-    } catch (err) {
-        console.error('Signup failed:', err);
-        res.status(500).json({ error: 'Signup failed' });
-    }
-});
-
-
-
-
-app.post('/login', async (req, res) => {
-    try {
-        const { email, password } = req.body;
-
-        console.log('Login request received');
-        console.log('Email:', email);
-
-        const user = await User.findOne({ email });
-
-        if (!user) {
-            console.log('User not found');
-            return res.status(401).json({ error: 'Invalid credentials' });
+            console.log('User signed up successfully:', savedUser);
+            res.status(201).json({ message: 'Signup successful' });
+        } catch (err) {
+            console.error('Signup failed:', err);
+            res.status(500).json({ error: 'Signup failed' });
         }
+    });
 
-        console.log('Input Password:', password);
-        console.log('Hashed Password from DB:', user.password);
+    app.post('/login', async (req, res) => {
+        try {
+            const { email, password } = req.body;
 
-        const passwordMatch = await bcrypt.compare(password, user.password);
+            console.log('Login request received');
+            console.log('Email:', email);
 
-        if (!passwordMatch) {
-            console.log('Password does not match');
-            return res.status(401).json({ error: 'Invalid credentials' });
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                console.log('User not found');
+                return res.status(401).json({ error: 'Invalid credentials' });
+            }
+
+            console.log('Input Password:', password);
+            console.log('Hashed Password from DB:', user.password);
+
+            const passwordMatch = await bcrypt.compare(password, user.password);
+
+            if (!passwordMatch) {
+                console.log('Password does not match');
+                return res.status(401).json({ error: 'Invalid credentials' });
+            }
+
+            console.log('Login successful');
+            res.json({ message: 'Login successful' });
+        } catch (error) {
+            console.error('Login failed:', error);
+            res.status(500).json({ error: 'Login failed' });
         }
-
-        console.log('Login successful');
-        res.json({ message: 'Login successful' });
-    } catch (error) {
-        console.error('Login failed:', error);
-        res.status(500).json({ error: 'Login failed' });
-    }
-});
-
-
-
-
+    });
 
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
