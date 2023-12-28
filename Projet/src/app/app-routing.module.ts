54b3c961b22919f71e1d6ac0c21930a60344ebd5
hadil <hadilyahiaoui02@gmail.com>
2023-12-28 01:05:54 +0100

    Update app.component files

--- Projet/src/app/app-routing.module.ts
+++ Projet/src/app/app-routing.module.ts
@@ -1,24 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { LandingHeaderComponent } from './landing-header/landing-header.component';
+import { LandingContainerComponent } from './landing-container/landing-container.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactusComponent } from './contactus/contactus.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { ProfileComponent } from './profile/profile.component';
 import { HelpingComponent } from './helping/helping.component';
-import { LandingContainerComponent } from './landing-container/landing-container.component';
-import { LandingHeaderComponent } from './landing-header/landing-header.component';
-import { LoginComponent } from './login/login.component';
-import { ContactusComponent } from './contactus/contactus.component';
-import { SignupComponent } from './signup/signup.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { EditProjectComponent } from './edit-project/edit-project.component';
 
 const routes: Routes = [
+  { path: 'signup', component: SignupComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'landing-header', component: LandingHeaderComponent},
+  { path: 'landing-container', component: LandingContainerComponent},
+  { path: 'footer', component: FooterComponent},
+  { path: 'contact-us', component: ContactusComponent},
   { path: 'sidebar', component: SidebarComponent },
   { path: 'profile', component: ProfileComponent },
   { path: 'helping', component: HelpingComponent },
-  { path: 'landing-container', component: LandingContainerComponent},
-  { path: 'landing-header', component: LandingHeaderComponent},
-  { path: 'login', component: LoginComponent},
-  { path: 'contact-us', component: ContactusComponent},
-  { path: 'signup', component: SignupComponent}
-
+  { path: 'home', component: HomeComponent},
+  { path: 'projects', component: ProjectsComponent},
+  { path: 'tasks/:projectId', component: TasksComponent}, 
+  { path: 'settings/:projectId', component: EditProjectComponent}
 ];
 
 @NgModule({
--- Projet/src/app/app.module.ts
+++ Projet/src/app/app.module.ts
@@ -1,46 +1,51 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-
-
-
 import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { LandingHeaderComponent } from './landing-header/landing-header.component';
+import { LandingContainerComponent } from './landing-container/landing-container.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { ProfileComponent } from './profile/profile.component';
 import { HelpingComponent } from './helping/helping.component';
-import { ContactusComponent } from './contactus/contactus.component';
-import { FooterComponent } from './footer/footer.component';
-import { LandingContainerComponent } from './landing-container/landing-container.component';
-import { LandingHeaderComponent } from './landing-header/landing-header.component';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
-import { ReactiveFormsModule } from '@angular/forms';
-
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { EditProjectComponent } from './edit-project/edit-project.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    SignupComponent,
+    LoginComponent,
+    LandingHeaderComponent,
+    LandingContainerComponent,
+    FooterComponent,
+    ContactusComponent,
+    ProjectsComponent,
     SidebarComponent,
     ProfileComponent,
     HelpingComponent,
-    ContactusComponent,
-    FooterComponent,
-    LandingContainerComponent,
-    LandingHeaderComponent,
-    LoginComponent,
-    SignupComponent
+    DashboardComponent,
+    HomeComponent,
+    TasksComponent,
+    EditProjectComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule,
     FormsModule,
     HttpClientModule
-
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
