e2c658e60c2d4151c5146ba4dfdd06979e13b6c8
hadil <hadilyahiaoui02@gmail.com>
2024-01-02 02:59:14 +0100

    Updated create-project page

--- Projet/src/app/create-project/create-project.component.ts
+++ Projet/src/app/create-project/create-project.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-project',
@@ -6,5 +8,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-project.component.css']
 })
 export class CreateProjectComponent {
+
+  project = {
+    name: '',
+    category: '',
+    status: '', 
+    lead: '',
+    description: ''
+  }
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  ngOnInit(): void {}
+
+  createProject(): void {
+    const projectData = {
+      name: this.project.name,
+      category: this.project.category,
+      status: this.project.status,
+      lead: this.project.lead,
+      description: this.project.description
+    };
+
+    const backendUrl = 'http://localhost:3000/create-project';
+
+    this.http.post(backendUrl, projectData)
+    .subscribe(
+      (response: any) => {
+        console.log('project creation successful:', response);
+        this.router.navigate(['/dashboard/projects']);
+      },
+      (error) => {
+        console.error('project creation failed:', error);
+      }
+    );
+  }
 
 }
