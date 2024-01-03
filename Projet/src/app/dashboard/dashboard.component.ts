3740fc413d21ac7c5d65c8dee26a48a8fda737e1
hadil <hadilyahiaoui02@gmail.com>
2024-01-03 04:25:50 +0100

    Updated dashboard

--- Projet/src/app/dashboard/dashboard.component.ts
+++ Projet/src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,18 +9,38 @@ import { Router } from '@angular/router';
 })
 export class DashboardComponent {
 
+  constructor(private router: Router, private route: ActivatedRoute) {}
 
-  constructor(private router: Router) {}
+  ngOnInit(): void {}
 
   logout(): void {
     this.router.navigate(['/landing']);
   }
 
-  create() : void {
+  createProject() : void {
     this.router.navigate(['/dashboard/create-project']);
+  }
+
+  createTask() : void {
+    // const projectId = this.route.snapshot.paramMap.get('projectId');
+    // console.log(projectId);
+    // this.router.navigate(['/dashboard', projectId, 'create-task']);
+    this.router.navigate(['/dashboard/create-task']);
+  }
+
+  addComment() : void {
+    this.router.navigate(['/dashboard/create-comment']);
   }
 
   isProjectsPage(): boolean {
     return this.router.isActive('/dashboard/projects', false);
+  }
+
+  isTasksPage(): boolean {
+    return this.router.url.endsWith('/tasks');
+  }
+
+  isCommentsPage(): boolean {
+    return this.router.isActive('/dashboard/comment', false);
   }
 }
