bb2068b2df12a8d5cd4ec6619c82f529aa946061
hadil <hadilyahiaoui02@gmail.com>
2024-01-04 14:39:06 +0100

    Updated comments page

--- Projet/src/app/comments/comments.component.ts
+++ Projet/src/app/comments/comments.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-comments',
@@ -11,26 +12,37 @@ export class CommentsComponent {
 
   comments: any[] = [];
 
-  constructor(private router: Router, private http: HttpClient) {}
+  projectId: string | null = null;
+  taskId: string | null = null;
+
+  constructor(private router: Router, private http: HttpClient, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.projectId = params['projectId'];
+      this.taskId = params['taskId'];
+
+      console.log('projectId:', this.projectId);
+      console.log('taskId:', this.taskId);
+    });
     this.loadComments();
   }
 
   loadComments(): void {
-    this.http.get<any[]>('http://localhost:3000/get-comments').subscribe(
-      (data) => {
-        this.comments = data;
-        console.log(this.comments);
-      },
-      (error) => {
-        console.error('Error fetching comments:', error);
-      }
-    );
+    this.http.get<any[]>(`http://localhost:3000/get-comments/${this.taskId}`).subscribe(
+    (data) => {
+      this.comments = data;
+      console.log(this.comments);
+    },
+    (error) => {
+      console.error('Error fetching comments:', error);
+    });
   }
 
-  navigateToSettings(): void {
-    
+  navigateToSettings() : void {}
+
+  addComment() : void {
+    this.router.navigate(['/dashboard', this.projectId, this.taskId, 'create-comment']);
   }
 
 }
